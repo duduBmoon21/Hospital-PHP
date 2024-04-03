df73f7d652b6750c42537a522538e4df08bb0018
Kee'ol <140634996+duduBmoon21@users.noreply.github.com>
2024-04-04 00:46:32 +0300

    first commit

---
+++ backend/admin/assets/inc/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+            <?php $timestamp = time(); $currentDate = gmdate('Y-m-d', $timestamp); echo $currentDate ?> &copy; Hospital. Developed By <a href="https://ducodeverse.com/" target="_blank">DuCodeVerse.</a> </a>
+ </div>
+
+        </div>
+    </div>
+</footer>
---
+++ backend/admin/assets/inc/footer1.php
@@ -0,0 +1,3 @@
+<footer class="footer footer-alt">
+     <?php $timestamp = time(); $currentDate = gmdate('Y-m-d', $timestamp); echo $currentDate ?> &copy; Hospital. Developed By <a href="https://ducodeverse.com/" target="_blank">DuCodeVerse.</a> </a>
+</footer>
